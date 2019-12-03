88f7d8e6a4bc0abd2a36eeb6b8b60404d7e99fb5
Dick Lewan <rlewan@Dicks-iMac.local>
2019-12-03 09:43:05 -0700

    Files as given.

---
+++ app/src/main/java/com/onxmaps/playground/calculator/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.onxmaps.playground.calculator
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * Instructions: Create a calculator that can perform 5 operations:
+     * addition, subtraction, multiplication, division, and find the
+     * remainder (modulus). The calculator should only be able to
+     * work with 2 real numbers. You will need to display the operation
+     * that is currently selected, as well as display a solution and
+     * find a way to clear your result and start over. If you have
+     * extra time, keep and display a history of all operations done.
+     */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.plant(Timber.DebugTree())
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { _ ->
+            // TODO perform the selected calculation type
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // TODO: Handle action bar item clicks here. Change the
+        //  symbol of the operator to match the operation
+        //  that was selected. Pressing the "equals"
+        //  button should also perform that operation
+        return super.onOptionsItemSelected(item)
+    }
+}
